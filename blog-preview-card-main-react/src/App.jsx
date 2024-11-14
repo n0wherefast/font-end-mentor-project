7fe86543dddc84f866a9a0888fc6738a6be9ac6e
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2024-11-14 18:02:53 +0100

    social links

--- blog-preview-card-main-react/src/App.jsx
+++ blog-preview-card-main-react/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <div className=' w-full h-screen bg-amber-400 flex justify-center items-center'>   
-        <div className=' border-2   gap-4 border-black shadow-[5px_5px_black]  bg-white w-72 md:w-80 min-h-[52vh] flex  justify-start items-center flex-col rounded-xl  '>
-          <img className='w-[90%]  m-5 rounded-xl' src={imgCard} alt="" />
+        <div className=' border  gap-4 border-black shadow-[8px_8px_black]  bg-white w-72 md:w-96 min-h-[55vh] flex  justify-start items-center flex-col rounded-xl  '>
+          <img className='w-[88%]  m-5 rounded-xl' src={imgCard} alt="" />
           <div className='w-full flex  flex-col pl-4 gap-2 font-bold'>
             <div className='bg-amber-400 rounded-md  w-24 h-8 flex justify-center items-center' >Learning</div>
             <p className='text-sm'>Published 21 Dec 2023</p>
@@ -16,10 +16,10 @@ function App() {
           </div>
            
            
-            <p className='pl-4 text-sm'> These languages are the backbone of every website, defining structure, content, and presentation.</p>
-            <div className='w-full p-4 font-bold flex items-center gap-3'> 
+            <p className='pl-4'> These languages are the backbone of every website, defining structure, content, and presentation.</p>
+            <div className='w-full p-2 font-bold flex items-center gap-3'> 
               <img className='w-10 h-10' src={avatar} alt="" />
-               <p>Greg Hooper</p>
+               <p className='text-sm'>Greg Hooper</p>
             </div>
         </div>
       </div>
---
+++ social-links-react/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import usrImg from './assets/images/avatar-jessica.jpeg'
+
+function App() {
+   const links = [ 'GitHub','Frontend Mentor','LinkedIn','Twitter','Instagram']
+
+  return (
+    <>
+      <div className=' flex  justify-center items-center w-full h-screen bg-[#141414]' >
+        <div className=' rounded-lg w-[22rem]  min-h-[60vh] bg-[#1f1f1f] flex flex-col justify-around items-center ' >
+          <div className=' text-white gap-1 w-full h-[40%] flex flex-col justify-start items-center ' > 
+            <img className=' my-5 w-20 rounded-full' src={usrImg} alt="" />
+            <h1 className=' text-2xl font-bold'>  Jessica Randall </h1>
+            <p className='mb-4 text-sm  font-bold text-lime-400'>   London, United Kingdom </p>
+            <p className=' text-sm'> &quot;Front-end developer and avid reader.&quot;   </p>
+          </div>
+          <div className='w-full flex items-center flex-col gap-3 m-2 text-white font-bold '>
+           {links.map((link,idx)=>(
+            <p key={idx} className='flex justify-center items-center rounded-lg bg-[#333] hover:bg-lime-400 hover:text-[#333] w-[85%] h-10 '>{link}</p>
+           ))} 
+          </div>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
